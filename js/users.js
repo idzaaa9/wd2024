04435a1414286d2bf3d1f37772e61b5f3d7bed56
idzaaa9 <jordanovski.ilija@jjzmaj.edu.rs>
2024-05-25 09:58:00 +0200

    feat: implemented script for the user listing

---
+++ js/users.js
@@ -0,0 +1,112 @@
+var firebaseUrl = 'https://festivaliwd-default-rtdb.europe-west1.firebasedatabase.app';
+
+document.addEventListener('DOMContentLoaded', function () {
+  
+  var request = new XMLHttpRequest();
+
+  const container = document.getElementById('user-table');
+
+  request.onreadystatechange = function () {
+    if (this.readyState == 4) {
+
+      if (this.status != 200) {
+        alert('Error occured!');
+        return;
+      }
+
+      var responseData = JSON.parse(request.responseText);
+
+      for (var userId in responseData) {
+        var user = responseData[userId];
+        container.appendChild(createRow(user, userId));
+      }
+    }
+  }
+
+  request.open('GET', firebaseUrl + '/korisnici.json');
+  request.send();
+});
+
+function createRow (data, id) {
+  const row = document.createElement('tr');
+
+  const usernameTd = document.createElement('td');
+    usernameTd.textContent = data.korisnickoIme;
+  row.appendChild(usernameTd);
+
+  const passwordTd = document.createElement('td');
+    passwordTd.textContent = data.lozinka;
+  row.appendChild(passwordTd);
+
+  const nameTd = document.createElement('td');
+    nameTd.textContent = data.ime;
+  row.appendChild(nameTd);
+
+  const surnameTd = document.createElement('td');
+    surnameTd.textContent = data.prezime;
+  row.appendChild(surnameTd);
+
+  const emailTd = document.createElement('td');
+    emailTd.textContent = data.email;
+  row.appendChild(emailTd);
+
+  const birthdayTd = document.createElement('td');
+    birthdayTd.textContent = data.datumRodjenja;
+  row.appendChild(birthdayTd);
+
+  const addressTd = document.createElement('td');
+    addressTd.textContent = data.adresa;
+  row.appendChild(addressTd);
+
+  const numberTd = document.createElement('td');
+    numberTd.textContent = data.telefon;
+  row.appendChild(numberTd);
+
+  const occupationTd = document.createElement('td');
+    occupationTd.textContent = data.zanimanje;
+  row.appendChild(occupationTd);
+
+  const editButtonTd = document.createElement('td');
+    const editButtonContainer = document.createElement('a');
+    editButtonContainer.href = "editUser.html/" + id;
+    const editButton = document.createElement('button');
+    editButton.classList.add('button');
+    editButton.textContent = 'Edit';
+
+    editButtonContainer.appendChild(editButton);
+    editButtonTd.appendChild(editButtonContainer);
+  row.appendChild(editButtonTd);
+
+  const deleteButtonTd = document.createElement('td');
+    const deleteButtonContainer = document.createElement('a');
+    deleteButtonContainer.onclick = deleteUser(id); // TODO: implement deletion 
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button');
+    deleteButton.textContent = 'Delete';
+
+    deleteButtonContainer.appendChild(deleteButton);
+    deleteButtonTd.appendChild(deleteButtonContainer);
+  row.appendChild(deleteButtonTd);
+
+
+  return row;
+}
+
+function deleteUser(userId) {
+
+  var request = new XMLHttpRequest();
+
+  request.open('DELETE', firebaseUrl + /korisnici/ + userId + '.json', true);
+
+  request.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      if (this.status != 200) {
+        alert('Error while deleting user');
+        return;
+      }
+      alert('User deleted successfully');
+    }
+  }
+
+  request.send();
+}
